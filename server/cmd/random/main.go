0e8d3de51ad46bfba523fce498a1ba7fa66deb57
Evgeniy Roslyackov <eveniy.roslyackov@gmail.com>
2023-11-22 10:28:54 +0300

    Add server.go

--- server/cmd/random/main.go
+++ server/cmd/random/main.go
@@ -1,4 +1,20 @@
 package main
 
-type Implementation struct {
+import (
+	"flag"
+	"github.com/passsquale/grpc/server/internal/config"
+	"github.com/passsquale/grpc/server/internal/server"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	if err := config.ReadConfigYML("config.yml"); err != nil {
+		log.Fatal().Err(err).Msg("Failed init configuration")
+	}
+	cfg := config.GetConfigInstance()
+	flag.Parse()
+	if err := server.NewGrpcServer().Start(&cfg); err != nil {
+		log.Error().Err(err).Msg("Failed creating gRPC server")
+		return
+	}
 }
